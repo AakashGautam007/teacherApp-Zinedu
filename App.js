dc8092bb0d6acd82d55ecd37d1fa8d223a89f6b8
Aakash <aakashgtm007@gmail.com>
2022-02-03 16:59:17 +0530

    Vimeo Video

--- App.js
+++ App.js
@@ -1,76 +1,84 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React,{useEffect,useState,useMemo,useContext, useReducer} from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, LogBox, Alert } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import DrawerNav from './src/nav/DrawerNav'
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+  useContext,
+  useReducer,
+} from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  LogBox,
+  Alert,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import DrawerNav from "./src/nav/DrawerNav";
 // import Doubt from './src/screens/doubtresponse/Doubt'
-import Login from './src/screens/auth/Login'
-import {AuthContext} from './src/utils/context'
-import {ENDPOINT} from './src/utils/config'
+import Login from "./src/screens/auth/Login";
+import { AuthContext } from "./src/utils/context";
+import { ENDPOINT } from "./src/utils/config";
 import InternetConnectionAlert from "react-native-internet-connection-alert";
-import Toast from 'react-native-simple-toast';
-import AuthStack from './src/nav/AuthStack';
-import { requestUserPermission, notificationListner } from './src/utils/notificationService'
+import Toast from "react-native-simple-toast";
+import AuthStack from "./src/nav/AuthStack";
+import {
+  requestUserPermission,
+  notificationListner,
+} from "./src/utils/notificationService";
 
 LogBox.ignoreAllLogs();
 
 export default function App() {
-
-
-  const initaialLoginState={
-    isLoading:true,
-    userName:null,
-    userToken:null,
+  const initaialLoginState = {
+    isLoading: true,
+    userName: null,
+    userToken: null,
     // fcmToken:null
   };
-  
-  const loginReducer =(prevState,action)=>{
-    switch(action.type){
-      case 'RETRIVE_TOKEN':
-        return {...prevState,
-        isLoading:false,
-        userToken:action.token,
-      };
-      case 'SIGNIN':
-        return {...prevState,
-          userToken:action.token,
-          userName:action.username,
-          isLoading:false};
-        case 'LOGOUT':
-        return {...prevState,
-          userToken:null,
-          userName:null,
-          isLoading:false};
+
+  const loginReducer = (prevState, action) => {
+    switch (action.type) {
+      case "RETRIVE_TOKEN":
+        return { ...prevState, isLoading: false, userToken: action.token };
+      case "SIGNIN":
+        return {
+          ...prevState,
+          userToken: action.token,
+          userName: action.username,
+          isLoading: false,
+        };
+      case "LOGOUT":
+        return {
+          ...prevState,
+          userToken: null,
+          userName: null,
+          isLoading: false,
+        };
     }
   };
-  
-  
-  const [loginState,dispatch]=useReducer(loginReducer,initaialLoginState)
-  
-  
-  const authContext=useMemo(()=>({
-  
-    signin: async(username,password)=>{
-  
-      
-      const response = await fetch(
-        `${ENDPOINT}/login/`,
-        {
-          method: 'POST',
+
+  const [loginState, dispatch] = useReducer(loginReducer, initaialLoginState);
+
+  const authContext = useMemo(
+    () => ({
+      signin: async (username, password) => {
+        const response = await fetch(`${ENDPOINT}/login/`, {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({
             username: username,
             password: password,
             returnSecureToken: true,
             // service_provider:'zinedu'
-          })
+          }),
         });
-        
-  
+
         // if (!response.ok) {
         //   const errorResData = await response.json();
         //   const errorId = errorResData.error.message;
@@ -82,7 +90,7 @@ export default function App() {
         //   }
         //   throw new Error(errorId);
         // }
-        
+
         // const resData = await response.json();
         //  console.log(resData);
         //  console.log(resData.token)
@@ -96,16 +104,15 @@ export default function App() {
         //   console.log(e)
         // }
 
-
-        if(response.status===400){
-          Alert.alert("Invalid credentials")
+        if (response.status === 400) {
+          Alert.alert("Invalid credentials");
           // Toast.show(`Invalid credentials`, Toast.SHORT);
         }
-  
+
         // if (!response.ok) {
-          
+
         //   const errorId = errorResData.error.message;
-          
+
         //   if (errorId === 'EMAIL_NOT_FOUND') {
         //     message = 'This email could not be found!';
         //   } else if (errorId === 'INVALID_PASSWORD') {
@@ -115,117 +122,101 @@ export default function App() {
         //   throw new Error(errorId);
         // }
 
-        
-        if(response.ok){
+        if (response.ok) {
           const resData = await response.json();
-         console.log(resData);
-         console.log(resData.token)
-        const token = await resData.token;
-          if (token!==undefined)
+          console.log(resData);
+          console.log(resData.token);
+          const token = await resData.token;
+          if (token !== undefined)
+            try {
+              await AsyncStorage.setItem("userToken", resData.token);
+              await AsyncStorage.setItem("userName", username);
+              requestUserPermission(resData.token);
+            } catch (e) {
+              // saving error
+              console.log(e);
+            }
+
+          // setUserToken(resData.token)
+          // setIsLoading(false)
+          dispatch({ type: "SIGNIN", username: username, token: token });
+          Toast.show("Logged In", Toast.SHORT);
+        }
+      },
+
+      logout: async () => {
         try {
-          await AsyncStorage.setItem('userToken', resData.token)
-          await AsyncStorage.setItem('userName', username)
-          requestUserPermission(resData.token)
+          await AsyncStorage.removeItem("userToken");
         } catch (e) {
           // saving error
-          console.log(e)
+          console.log(e);
         }
-  
-        // setUserToken(resData.token)
+
+        dispatch({ type: "LOGOUT" });
+        // setUserToken(null)
         // setIsLoading(false)
-          dispatch({type:'SIGNIN',username:username,token:token})
-          Toast.show('Logged In', Toast.SHORT);}
-  },
-  
-  logout:async()=>{
-  
-    try {
-      await AsyncStorage.removeItem('userToken')
-    } catch (e) {
-      // saving error
-      console.log(e)
-    }
-  
-    dispatch({type:'LOGOUT'})
-  // setUserToken(null)
-  // setIsLoading(false)
-  Toast.show('Logged Out', Toast.SHORT);
-  }
-  
-  }),[]);
+        Toast.show("Logged Out", Toast.SHORT);
+      },
+    }),
+    []
+  );
 
-  const init = () =>{
-    setTimeout(async()=>{
-  
-      let userToken = null
-      let userName = null
+  const init = () => {
+    setTimeout(async () => {
+      let userToken = null;
+      let userName = null;
       try {
-       
-       userToken = await AsyncStorage.getItem('userToken')
-       userName = await AsyncStorage.getItem('userName')
-
+        userToken = await AsyncStorage.getItem("userToken");
+        userName = await AsyncStorage.getItem("userName");
       } catch (e) {
         // saving error
-        console.log(e)
+        console.log(e);
       }
-      dispatch({type:'RETRIVE_TOKEN',username:userName,token:userToken})
+      dispatch({ type: "RETRIVE_TOKEN", username: userName, token: userToken });
       // setIsLoading(false)
-    },1000);
-  }
-  
-  
+    }, 1000);
+  };
+
   useEffect(() => {
-    init()
+    init();
   }, []);
-  
-  
-  
-  if(loginState.isLoading){
+
+  if (loginState.isLoading) {
     // if(isLoading){
-    return(
-      <View style={{flex:1,justifyContent:'center',alignItems:'center'}} >
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator size="large" />
       </View>
-    )
+    );
   }
-
-
-
-
 
   return (
-
     <InternetConnectionAlert
-  onChange={(connectionState) => {
-    console.log("Connection State: ", connectionState);
-  }}
->
-  {/* {... Your whole application should be here ... } */}
-<AuthContext.Provider value={authContext} >
-<NavigationContainer>
-<StatusBar style="auto" hidden={true} />
-  {(loginState.userToken!==null) ?(
-  <DrawerNav/>
-  // <Doubt/>
-  ):
-
- <AuthStack/>
-
-  }
-</NavigationContainer>
-</AuthContext.Provider>
-</InternetConnectionAlert>
-
-
-
+      onChange={(connectionState) => {
+        console.log("Connection State: ", connectionState);
+      }}
+    >
+      {/* {... Your whole application should be here ... } */}
+      <AuthContext.Provider value={authContext}>
+        <NavigationContainer>
+          <StatusBar style="auto" hidden={true} />
+          {loginState.userToken !== null ? (
+            <DrawerNav />
+          ) : (
+            // <Doubt/>
+            <AuthStack />
+          )}
+        </NavigationContainer>
+      </AuthContext.Provider>
+    </InternetConnectionAlert>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/screens/pastliveclasses/ViewRecording.js
+++ src/screens/pastliveclasses/ViewRecording.js
@@ -1,94 +1,350 @@
-import React, { useEffect, useState, useCallback, useRef } from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+// import React, { useEffect, useState, useCallback, useRef } from 'react'
+// import { StyleSheet, Text, View, Button } from 'react-native'
+// import YoutubePlayer from "react-native-youtube-iframe";
+// import {width,height} from '../../utils/config'
+// import { Video, AVPlaybackStatus } from 'expo-av';
+// import getVideoId from 'get-video-id';
+
+// const ViewRecording = ({navigation, route}) => {
+
+//     const {link} = route.params
+//     // https://www.youtube.com/watch?v=HOYpAgWPTps
+//     // https://classrecording.s3.ap-south-1.amazonaws.com/45.mp4
+
+//     const l2 = link.split('/')
+
+//     console.log(link)
+
+//     const { id } = getVideoId(link);
+
+//     const [playing, setPlaying] = useState(false);
+
+//     const video = React.useRef(null);
+//     const [status, setStatus] = React.useState({});
+
+//     const onStateChange = useCallback((state) => {
+//       if (state === "ended") {
+//         setPlaying(false);
+//         Alert.alert("video has finished playing!");
+//       }
+//     }, []);
+
+//     const togglePlaying = useCallback(() => {
+//       setPlaying((prev) => !prev);
+//     }, []);
+
+//     return (
+//         <View style={styles.container}>
+//               {l2[2]=='www.youtube.com'?<YoutubePlayer
+//         height={300}
+//         width={width}
+//         play={playing}
+//         videoId={id}
+//         onChangeState={onStateChange}
+//       />:
+//       // }{l2[2]=='classrecording.s3.ap-south-1.amazonaws.com'||'https://classrecording.s3.amazonaws.com'&&
+//        <Video
+//         ref={video}
+//         style={styles.video}
+//         source={{
+//           uri: link,
+//         }}
+//         useNativeControls
+//         resizeMode="contain"
+//         isLooping
+//         onPlaybackStatusUpdate={status => setStatus(() => status)}
+//       />}
+
+// {/* <View style={styles.buttons}>
+//         <Button
+//           title={status.isPlaying ? 'Pause' : 'Play'}
+//           onPress={() =>
+//             status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+//           }
+//         />
+//       </View> */}
+
+//         </View>
+//     )
+// }
+
+// export default ViewRecording
+
+// const styles = StyleSheet.create({
+//     container:{
+//         backgroundColor:'white',
+//         flex:1,
+//         justifyContent:'center',
+//         alignItems:'center'
+//     },
+//     buttons: {
+//       flexDirection: 'row',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//     },
+//     video: {
+//       alignSelf: 'center',
+//       width,
+//       height: width*0.7,
+//     },
+//     })
+import React, { useRef, useState, useEffect, useCallback } from "react";
+import { StyleSheet, Text, View, Button, Pressable } from "react-native";
+
+// import Vimeo from "react-native-vimeo";
+import getVideoId from "get-video-id";
+import Video from "react-native-video";
+import { height, width } from "../../utils/config";
+import WebView from "react-native-webview";
+// import * as ScreenOrientation from "expo-screen-orientation";
+import MediaControls, { PLAYER_STATES } from "react-native-media-controls";
 import YoutubePlayer from "react-native-youtube-iframe";
-import {width,height} from '../../utils/config'
-import { Video, AVPlaybackStatus } from 'expo-av';
-import getVideoId from 'get-video-id';
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const ViewRecording = ({navigation, route}) => {
+export default function PlayPastLiveClass({ navigation, route }) {
+  const { link } = route.params;
+  // https://www.youtube.com/watch?v=HOYpAgWPTps
+  // https://classrecording.s3.ap-south-1.amazonaws.com/45.mp4
+  console.log(link);
+  // https://player.vimeo.com/video/662609383?byline=false&portrait=false&autoplay=false&title=false&dnt=true
 
-    const {link} = route.params
-    // https://www.youtube.com/watch?v=HOYpAgWPTps
-    // https://classrecording.s3.ap-south-1.amazonaws.com/45.mp4
+  // const l2 = link.split("/");
 
-    const l2 = link.split('/')
+  const { id } = getVideoId(link);
 
+  const videoPlayer = useRef(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [paused, setPaused] = useState(false);
+  const [playerState, setPlayerState] = useState(PLAYER_STATES.PLAYING);
+  const [screenType, setScreenType] = useState("contain");
 
+  const onSeek = (seek) => {
+    //Handler for change in seekbar
+    videoPlayer.current.seek(seek);
+  };
 
-    console.log(link)
+  const onPaused = (playerState) => {
+    //Handler for Video Pause
+    setPaused(!paused);
+    setPlayerState(playerState);
+  };
 
-    const { id } = getVideoId(link);
+  const onReplay = () => {
+    //Handler for Replay
+    setPlayerState(PLAYER_STATES.PLAYING);
+    videoPlayer.current.seek(0);
+  };
 
-    const [playing, setPlaying] = useState(false);
+  const onProgress = (data) => {
+    // Video Player will progress continue even if it ends
+    if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
+      setCurrentTime(data.currentTime);
+    }
+  };
 
-    const video = React.useRef(null);
-    const [status, setStatus] = React.useState({});
+  const onLoad = (data) => {
+    setDuration(data.duration);
+    setIsLoading(false);
+  };
 
-    const onStateChange = useCallback((state) => {
-      if (state === "ended") {
-        setPlaying(false);
-        Alert.alert("video has finished playing!");
-      }
-    }, []);
-  
-    const togglePlaying = useCallback(() => {
-      setPlaying((prev) => !prev);
-    }, []);
-  
-    
+  const onLoadStart = (data) => setIsLoading(true);
 
-    return (
-        <View style={styles.container}>
-              {l2[2]=='www.youtube.com'?<YoutubePlayer
-        height={300}
-        width={width}
-        play={playing}
-        videoId={id}
-        onChangeState={onStateChange}
-      />:
-      // }{l2[2]=='classrecording.s3.ap-south-1.amazonaws.com'||'https://classrecording.s3.amazonaws.com'&&
-       <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: link,
-        }}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
-      />}
+  const onEnd = () => setPlayerState(PLAYER_STATES.ENDED);
 
-{/* <View style={styles.buttons}>
-        <Button
-          title={status.isPlaying ? 'Pause' : 'Play'}
-          onPress={() =>
-            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-          }
+  const onError = () => alert("Oh! ", error);
+
+  const renderToolbar = () => (
+    <View>
+      <Text style={styles.toolbar}> toolbar </Text>
+    </View>
+  );
+
+  const onSeeking = (currentTime) => setCurrentTime(currentTime);
+
+  const exitFullScreen = () => {
+    alert("Exit full screen");
+  };
+
+  const enterFullScreen = () => {};
+
+  const onFullScreen = () => {
+    setIsFullScreen(isFullScreen);
+    if (screenType == "contain") setScreenType("cover");
+    else setScreenType("contain");
+  };
+
+  const onFullscreenUpdate = async ({ fullscreenUpdate }) => {
+    switch (fullscreenUpdate) {
+      case Video.FULLSCREEN_UPDATE_PLAYER_DID_PRESENT:
+        await ScreenOrientation.lockAsync(
+          ScreenOrientation.OrientationLock.LANDSCAPE_LEFT
+        );
+        // await ScreenOrientation.unlockAsync(
+        //   ScreenOrientation.OrientationLock.LANDSCAPE
+        // );
+        console.log("Lanscape");
+        // only on Android required
+        break;
+      case Video.FULLSCREEN_UPDATE_PLAYER_WILL_DISMISS:
+        await ScreenOrientation.lockAsync(
+          ScreenOrientation.OrientationLock.PORTRAIT
+        ); // only on Android required
+        break;
+    }
+  };
+
+  const showVideoInFullscreen = async () => {
+    await videoPlayer.current.presentFullscreenPlayer();
+  };
+
+  const onStateChange = useCallback((state) => {
+    if (state === "ended") {
+      setPlaying(false);
+      Alert.alert("video has finished playing!");
+    }
+    if (state === "") {
+    }
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {link.toString().includes("vimeo") ? (
+        <WebView
+          source={{
+            uri: `https://player.vimeo.com/video/${id}?byline=false&portrait=false&autoplay=false&title=false&dnt=true`,
+          }}
         />
-      </View> */}
-       
+      ) : (
+        // <Vimeo
+        //   videoId={id}
+        //   style={{ backgroundColor: "black" }}
+        //   onReady={() => console.log("Video is ready")}
+        //   onPlay={() => {
+        //     console.log("Video is playing");
+        //   }}
+        //   onFinish={() => console.log("Video is finished")}
+        //   loop={false}
+        //   controls={true}
+        //   speed={false}
+        //   time={"0m0s"}
+        //   onVolumeChange={10}
+        //   scalesPageToFit={false}
+        // />
+        <></>
+      )}
+
+      {link.toString().includes("youtu") ? (
+        <View>
+          <YoutubePlayer
+            ref={videoPlayer}
+            height={350}
+            width={width}
+            initialPlayerParams={{
+              start: true,
+              controls: true,
+              preventFullScreen: false,
+              modestbranding: true,
+              loop: true,
+            }}
+            webViewProps={{ contentMode: "recommended" }}
+            videoId={id}
+            onChangeState={onStateChange}
+          />
+          <Pressable
+            // TouchableOpacity to "steal" taps
+            // absolutely positioned to the top
+            // height must be adjusted to
+            // just cover the top 3 dots
+            style={{
+              top: 0,
+              height: 100,
+              width: "100%",
+              position: "absolute",
+            }}
+          />
+          <Pressable
+            // TouchableOpacity to "steal" taps
+            // absolutely positioned to the top
+            // height must be adjusted to
+            // just cover the top 3 dots
+            style={{
+              right: 0,
+              height: "100%",
+              width: width * 0.17,
+              position: "absolute",
+            }}
+          />
         </View>
-    )
+      ) : (
+        <></>
+      )}
+
+      {link.toString().includes(".mp4") ? (
+        <Video
+          onEnd={onEnd}
+          onLoad={onLoad}
+          onLoadStart={onLoadStart}
+          onProgress={onProgress}
+          paused={paused}
+          ref={videoPlayer}
+          resizeMode={screenType}
+          onFullScreen={onFullscreenUpdate}
+          source={{
+            uri: link,
+          }}
+          fullscreen={true}
+          style={styles.mediaPlayer}
+          volume={10}
+        />
+      ) : (
+        <></>
+      )}
+
+      {link.toString().includes(".mp4") ? (
+        <MediaControls
+          duration={duration}
+          isLoading={isLoading}
+          mainColor="#333"
+          onFullScreen={showVideoInFullscreen}
+          onPaused={onPaused}
+          onReplay={onReplay}
+          onSeek={onSeek}
+          onSeeking={onSeeking}
+          playerState={playerState}
+          progress={currentTime}
+          toolbar={renderToolbar()}
+        />
+      ) : (
+        <></>
+      )}
+    </View>
+  );
 }
 
-export default ViewRecording
-
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'white',
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    buttons: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    video: {
-      alignSelf: 'center',
-      width,
-      height: width*0.7,
-    },
-    })
+  container: {
+    flex: 1,
+    alignContent: "center",
+    // alignItems: "center",
+    justifyContent: "center",
+  },
+  toolbar: {
+    marginTop: 30,
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+  },
+  mediaPlayer: {
+    width: width,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: "black",
+    justifyContent: "center",
+  },
+});
